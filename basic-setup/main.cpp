6d5996cbd8f1177f3afb5de1fe5dd7b274452299
Markus Worchel <mworchel@hotmail.de>
2019-09-24 22:14:12 +0200

    Add enumerating physical devices to the vulkan test

--- basic-setup/main.cpp
+++ basic-setup/main.cpp
@@ -1,3 +1,4 @@
+#define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.hpp>
 
 #include <GLFW/glfw3.h>
@@ -20,6 +21,23 @@ int main()
     for (auto extension : instanceExtensions)
     {
         std::cout << extension.extensionName << std::endl;
+    }
+
+    vk::ApplicationInfo applicationInfo;
+    applicationInfo.setPApplicationName("Basic Setup");
+
+    vk::InstanceCreateInfo instanceInfo;
+    instanceInfo.setPApplicationInfo(&applicationInfo);
+
+    vk::Instance instance = vk::createInstance(instanceInfo);
+
+    auto physicalDevices = instance.enumeratePhysicalDevices();
+    
+    std::cout << "\nVulkan devices:" << std::endl;
+    for (auto physicalDevice : physicalDevices)
+    {
+        auto deviceProperties = physicalDevice.getProperties();
+        std::cout << deviceProperties.deviceName << std::endl;
     }
 
     // GLM test
